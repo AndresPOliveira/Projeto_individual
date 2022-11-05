275ef30f28dac916fc34eda6074c21f5f40d2b93
AndresPOliveira <andres.oliveira@sptech.school>
2022-11-05 17:51:02 -0300

    Adicionada uma forma de sair do modal login

--- js/login.js
+++ js/login.js
@@ -2,3 +2,7 @@ function logar() {
     let modal = document.querySelector('.login');
     modal.style.display = 'block';
 }
+function fechar(){
+    let modal = document.querySelector('.login')
+    modal.style.display = 'none';
+}
