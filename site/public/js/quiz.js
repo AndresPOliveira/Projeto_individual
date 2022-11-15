5addb980146219c6a60f23060e7cb6aebca269a8
andresPOliveira <andres.oliveira@sptech.school>
2022-11-15 17:48:38 -0300

    Lógica para direcionar para as paginas de resultado preenchida

--- site/public/js/quiz.js
+++ site/public/js/quiz.js
@@ -46,8 +46,45 @@ function chamarQuestao5() {
     esconderPergunta.style.display = 'none';
 }
 // Apartir daqui resultado do quiz
-var contador1 = 0;
-var contador2 = 0;
-var contador3 = 0;
-var contador4 = 0;
-var contador5 = 0;
+var index1 = 0;
+var index2 = 0;
+var index3 = 0;
+var index4 = 0;
+var index5 = 0;
+
+function vermelho() {
+    index1 ++;
+    console.log(index1,index2,index3,index4,index5)
+}
+function azul() {
+    index2 ++;
+    console.log(index1,index2,index3,index4,index5)
+}
+function preto() {
+    index3 ++;
+    console.log(index1,index2,index3,index4,index5)
+}
+function branco() {
+    index4 ++;
+    console.log(index1,index2,index3,index4,index5)
+}
+function verde() {
+    index5 ++;
+    console.log(index1,index2,index3,index4,index5)
+} 
+function resultado() {
+    console.log("entrou na função")
+    const paginaResultado = document.querySelector('.resultado')
+    if (index1 > index2 && index1 > index3 && index1 > index4 && index1 > index5) {
+        paginaResultado.href = 'cores/vermelho.html'
+        console.log("entrou no if")
+    } else if (index2 > index1 && index2 > index3 && index2 > index4 && index2 > index5) {
+        
+    } else if (index3 > index1 && index3 > index2 && index3 > index4 && index3 > index5){
+        
+    } else if (index4 > index1 && index4 > index3 && index4 > index2 && index4 > index5){
+
+    } else if (index5 > index1 && index5 > index3 && index5 > index4 && index5 > index2){
+
+    }
+}
