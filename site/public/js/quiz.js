5402a90cae8102902cc793e81c9cd94f44ea7300
andresPOliveira <andres.oliveira@sptech.school>
2022-11-15 14:13:34 -0300

    Botão de login consertado e iniciando os trabalhos no quiz

---
+++ site/public/js/quiz.js
@@ -0,0 +1,6 @@
+function chamarQuiz() {
+    let mostrarQuiz = document.querySelector('.quiz');
+    mostrarQuiz.style.display = 'block';
+    let esconderInicio = document.querySelector('.inicio')
+    esconderInicio.style.display = 'none';
+} 
